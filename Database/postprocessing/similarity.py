f314865430d6e04017cedc78f956391a03a40c58
Yilj <nigk@web.de>
2025-01-22 11:23:35 +0100

    Removed duplicate questions with cosine similarity > 0.999.

--- Database/postprocessing/similarity.py
+++ Database/postprocessing/similarity.py
@@ -33,12 +33,30 @@ plt.show()
 plt.hist(cosine_similarity.flatten(), bins=100)
 plt.show()
 
+# Set the threshold for cosine similarity
+threshold = 0.999
+
 for i in range(cosine_similarity.shape[0]):
     for j in range(cosine_similarity.shape[1]):
-        if i != j and cosine_similarity[i, j] > 0.9:
-            print("Similar pair of questions:")
-            print("Question 1:", extraction_data[keys[i]]["question"])
-            print("Question 2:", extraction_data[keys[j]]["question"])
-            print("Cosine similarity:", cosine_similarity[i, j])
+        if i != j and cosine_similarity[i, j] > threshold and i < j:
+            # print("Keys:", keys[i], "\n", keys[j])
+            # print("Similar pair of questions:")
+            # print("Question 1:", extraction_data[keys[i]]["question"])
+            # print("Question 2:", extraction_data[keys[j]]["question"])
+            # print("Cosine similarity:", cosine_similarity[i, j])
+            # Delete one of the duplicate questions
+            try:
+                embedding_data.pop(keys[j])
+                extraction_data.pop(keys[j])
+            except KeyError:
+                pass
+
+# Save the updated embedding data
+with open(".temp/embedding_batch_output.json", "w") as file:
+    json.dump(embedding_data, file)
+
+# Save the updated extraction data
+with open(".temp/extraction_batch_output.json", "w") as file:
+    json.dump(extraction_data, file)
 
 pass
